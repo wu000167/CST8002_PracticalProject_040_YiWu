0f0276c17b779d5376e3c08129017e4128843920
Yi Wu <wu000167@algonquinlive.com>
2025-09-20 02:14:43 -0400

    Add simple name banner

---
+++ project1/shname.py
@@ -0,0 +1,8 @@
+"""
+Small helper to keep the student's full name always visible on screen.
+"""
+
+def show_name_banner(full_name):
+    print("=" * 70)
+    print(" Student:", full_name)
+    print("=" * 70)
